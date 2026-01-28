400db4d80878951ca93a617c71320d741de7936e
pooja-yadav-py <mispooju@gmail.com>
2026-01-28 18:48:15 +0530

    config revert

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.unsplash.com',
-        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'img.youtube.com',
-        pathname: '/**',
       },
     ],
   },
